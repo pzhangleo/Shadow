1910e93e5a4dc28b11902c34a9165ba3d8d9cbf6
pzhang <pengzh.leo@gmail.com>
2019-11-27 15:19:05 +0800

    update some tools

---
+++ uploadmaven.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar migrate-local-repo-tool.jar -cd ~/.m2/repository/com/tencent -t "https://repo.rdc.aliyun.com/repository/111975-snapshot-adUwbB" -u vJ7Gor -p eiFE2dNr6t
